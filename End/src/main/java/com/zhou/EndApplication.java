c5e9316d9b5fe302c236a84498737c58f3d6b0e9
zhouzhoi <evelynhetun@gmail.com>
2023-08-10 20:36:09 +0800

    项目创建

---
+++ End/src/main/java/com/zhou/EndApplication.java
@@ -0,0 +1,13 @@
+package com.zhou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndApplication.class, args);
+    }
+
+}
