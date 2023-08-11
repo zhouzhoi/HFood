3147ee922e0bbf0ae02c63a1f9e9835529e7c38b
zhouzhoi <evelynhetun@gmail.com>
2023-08-11 10:04:05 +0800

    添加了user的增删改功能，并使用postman完成接口测试

--- End/src/main/java/com/zhou/controller/UserController.java
+++ End/src/main/java/com/zhou/controller/UserController.java
@@ -1,13 +1,12 @@
 package com.zhou.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.zhou.entity.User;
 import com.zhou.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +28,27 @@ public class UserController {
     @GetMapping("/list")
     public List<User> list(){
         return userService.list();
+    }
+
+    //新增
+    @PostMapping("/save")
+    public boolean save(@RequestBody User user){
+        return userService.save(user);
+    }
+    //修改
+    @PostMapping("/mod")
+    public boolean mod(@RequestBody User user){
+        return userService.updateById(user);
+    }
+    //新增或修改
+    @PostMapping("/saveOrMod")
+    public boolean saveOrMod(@RequestBody User user){
+        return userService.saveOrUpdate(user);
+    }
+    //删除
+    @GetMapping("/delete")
+    public boolean saveOrMod( Integer id){
+        return userService.removeById(id);
     }
 
 }
