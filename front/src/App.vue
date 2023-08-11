7d415cfbc0e374d42dd634359e06a369149bade2
zhouzhoi <evelynhetun@gmail.com>
2023-08-11 15:31:56 +0800

    导入element-ui，在main.js全局引入

---
+++ front/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <el-button type="warning">这是element按钮</el-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
