b0ee9f0bb8a99fae917a11078a57f8095f330168
zhouzhoi <evelynhetun@gmail.com>
2023-08-11 16:27:24 +0800

    使用container搭建页面布局

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="app">
-    <el-button type="warning">这是element按钮</el-button>
+  <HomePage></HomePage>
   </div>
 </template>
 
 <script>
 
+import HomePage from "./components/HomePage";
 export default {
   name: 'App',
   components: {
+    HomePage
 
   }
 }
@@ -16,11 +18,6 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
