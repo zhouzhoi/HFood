5a3f2533d4095f1e11cf30cf4a5ceeb9208bbf98
zhouzhoi <evelynhetun@gmail.com>
2023-08-11 09:27:41 +0800

    添加代码生成器，并使用代码生成器重新创建user的相关文件

--- End/src/main/java/com/zhou/controller/UserController.java
+++ End/src/main/java/com/zhou/controller/UserController.java
@@ -1,26 +1,34 @@
 package com.zhou.controller;
 
+
 import com.zhou.entity.User;
 import com.zhou.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author zhou
+ * @since 2023-08-11
+ */
 @RestController
-public class HelloController {
-    @GetMapping
-    public String hello(){
-        return "hello";
-    }
+@RequestMapping("/user")
+public class UserController {
 
     @Autowired
     private UserService userService;
 
     @GetMapping("/list")
     public List<User> list(){
-        return userService.listAll();
+        return userService.list();
     }
-}
 
+}
